15453e25da04a2ae71cc231ef86d4439c325ada6
npablo1987 <97960344+npablo1987@users.noreply.github.com>
2025-06-09 12:35:41 -0400

    Add redirect countdown on session errors

--- src/app/services/serviceui/mensaje-overlay.service.ts
+++ src/app/services/serviceui/mensaje-overlay.service.ts
@@ -15,6 +15,29 @@ export class MensajeOverlayService {
     }
   }
 
+  /**
+   * Muestra un mensaje con cuenta regresiva en segundos. La cuenta se
+   * actualiza cada segundo y, al finalizar, ejecuta el callback opcional.
+   */
+  showCountdown(msg: string, seconds: number, onFinish?: () => void) {
+    let restante = seconds;
+    const actualizar = () => {
+      this.show(`${msg} Redirigiendo en ${restante} s…`, 0);
+    };
+
+    actualizar();
+    const timer = setInterval(() => {
+      restante--;
+      if (restante <= 0) {
+        clearInterval(timer);
+        this.hide();
+        if (onFinish) { onFinish(); }
+      } else {
+        actualizar();
+      }
+    }, 1000);
+  }
+
   hide() {
     this.msgSubject.next(null);
   }
