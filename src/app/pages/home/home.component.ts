95efdb97e4155284332a1764ba9af8bcc4bb5ee9
npablo1987 <97960344+npablo1987@users.noreply.github.com>
2025-06-16 12:37:59 -0400

    feat: add dismissible alert banner

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router, NavigationEnd } from '@angular/router';
 import { HeaderadminComponent } from '../../components/headeradmin/headeradmin.component';
 import { PieadminComponent } from '../../components/pieadmin/pieadmin.component';
 import { FichaselecionadaService } from '../../services/session/fichaselecionada.service';
@@ -13,10 +13,20 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
   pendientes = 0;
+  showAlert = true;
 
-  constructor(private fichaSrv: FichaselecionadaService) {}
+  constructor(private fichaSrv: FichaselecionadaService, private router: Router) {}
 
   ngOnInit(): void {
     this.fichaSrv.getFichasPendientes$().subscribe(n => this.pendientes = n);
+    this.router.events.subscribe(ev => {
+      if (ev instanceof NavigationEnd) {
+        this.showAlert = true;
+      }
+    });
+  }
+
+  dismissAlert(): void {
+    this.showAlert = false;
   }
 }
