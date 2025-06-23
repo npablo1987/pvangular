dff62a897888f2007a1d890503bab894a5db9b81
npablo1987 <97960344+npablo1987@users.noreply.github.com>
2025-06-23 15:58:47 -0400

    Add JWT interceptor and improve user system UI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http';
 import { ForceHttpsInterceptor } from './interceptors/force-https.interceptor';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: ForceHttpsInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
   ]
